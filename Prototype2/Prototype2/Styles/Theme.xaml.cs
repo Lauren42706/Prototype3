a22f0823bd84362c3c1b5d3ec52b7acc098de8fe
Lauren42706 <4270683@myuwc.ac.za>
2022-07-22 13:37:46 +0200

    Fixed the Themes

--- Prototype2/Prototype2/Styles/Theme.xaml.cs
+++ Prototype2/Prototype2/Styles/Theme.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace Prototype2.Styles
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Theme : ContentPage
+    public partial class Theme : ResourceDictionary
     {
         public Theme()
         {
