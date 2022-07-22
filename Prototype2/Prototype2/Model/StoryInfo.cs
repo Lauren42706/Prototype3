e5e921766bdb1a788533b759485d4ec31adcad32
Samkelisiwe02 <102726552+Samkelisiwe02@users.noreply.github.com>
2022-07-22 15:09:24 +0200

    ViewModel

---
+++ Prototype2/Prototype2/Model/StoryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype2.Model
+{
+    public partial class StoryInfo
+    {
+        public class CharacterInfo
+        {
+            public string CharacterName { get; set; }
+            
+        }
+        public class BackgroundInfo
+        {
+            public string BackgroundName { get; set; }
+        }
+    }
+}
