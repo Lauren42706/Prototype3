cbcc2248f188f3f1c438798edd52b31e0a8b11f6
Mihlali <4280734@myuwc.ac.za>
2022-07-22 15:05:53 +0200

    added Converters

---
+++ Prototype2/Prototype2/Converters/FirstValidationErrorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prototype2.Converters
+{
+    public class FirstValidationErrorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isDone = (bool)value;
+            return isDone ? "\uf058" : "\uf111";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
