c5ccb0908835721829766e2432901fcca14b0db6
Samkelisiwe02 <102726552+Samkelisiwe02@users.noreply.github.com>
2022-07-22 16:27:15 +0200

    commit

--- Prototype2/Prototype2/Model/StoryInfo.cs
+++ Prototype2/Prototype2/Model/StoryInfo.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Prototype2.Model
 {
     public partial class StoryInfo
     {
+        public static implicit operator ObservableCollection<object>(StoryInfo v)
+        {
+            throw new NotImplementedException();
+        }
+
         public class CharacterInfo
         {
             public string CharacterName { get; set; }
---
+++ Prototype2/Prototype2/ViewModel/StoryPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prototype2.Model;
+using Prism.Commands;
+using Prism.Navigation;
+using System.Collections.ObjectModel;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype2.ViewModel
+{
+    public class StoryPageViewModel : ViewModelBase
+    {
+        private StoryInfo _character;
+        private ObservableCollection<StoryInfo> _characterItems;
+        public ObservableCollection<StoryInfo> CharacterItems
+        {
+            get { return _characterItems; }
+            set { SetProperty(ref _characterItems, value); }
+
+        }
+        private DelegateCommand<StoryInfo> _dragCharacterCommand;
+  
+    
+
+       
+        public StoryPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "StoryMaker";
+        }
+
+    }
+}
