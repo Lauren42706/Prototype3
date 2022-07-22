f58f93e259cdef10c62b151d1e48caf2eb9505bc
Lauren42706 <4270683@myuwc.ac.za>
2022-07-22 16:03:46 +0200

    Added quitea bit of helpers and an error dialog. Also added a loginviewmodel

---
+++ Prototype2/Prototype2/View/Dialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype2.View.Dialogs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ErrorDialog : ContentPage
+    {
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
