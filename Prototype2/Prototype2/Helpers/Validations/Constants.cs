8a053b91033d2c3ec48f10fdee07807ad8d00197
Lauren42706 <4270683@myuwc.ac.za>
2022-07-22 13:17:45 +0200

    Constants

---
+++ Prototype2/Prototype2/Helpers/Validations/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype2.Helpers.Validations
+{
+    public static class Constants
+    {
+
+        public static class Errors
+        {
+            public static string GeneralError = "Something went wrong! Please wait a moment and try again.";
+            public static string WrongUserOrPinError = "The email or pin is incorrect";
+        }
+    }
+}
